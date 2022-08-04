9ce94127c44e1b3ff5a56542a834592951484b94
kaellumb <kaellum@bean.im>
2022-08-04 11:14:32 +0100

    COmpletely redid the project display area

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-var myfunc = setInterval(function() {
-    var now = new Date().getTime();
-    now = Math.floor(now / 1000);
-    document.getElementById("time").innerHTML = now.toLocaleString();
-    }, 1000)
+const scrollList = document.getElementById('scroll-list'),
+    btnLeft = document.getElementById("scrollLeft"),
+    btnRight = document.getElementById("scrollRight");
+
+let scrollSpeed = 200;
+
+var x = window.matchMedia("(max-width: 900px)");
+
+
+function changeScrollSpeed(x) {
+    if (x.matches) {
+        scrollSpeed = 200;
+    } else scrollSpeed = 600;    
+}
+
+changeScrollSpeed(x);
+
+x.addEventListener("change", () => {
+    changeScrollSpeed(x)
+});
+
+function scrollRight() {
+    scrollList.scrollBy({
+        left: scrollSpeed,
+        behavior: 'smooth'
+    });
+}
+
+function scrollLeft() {
+    scrollList.scrollBy({
+        left: -scrollSpeed,
+        behavior: 'smooth'
+    });
+}
+
+btnLeft.onclick = function () {
+    scrollLeft();
+};
+btnRight.onclick = function () {
+    scrollRight();
+};
